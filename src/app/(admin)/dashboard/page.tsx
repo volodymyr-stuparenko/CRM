3a77933b19fcd47c6b6c3e90f6c470672b8b7758
Volodymyr <StuparenkoVladimir@gmail.com>
2025-09-30 15:13:13 +0300

    Add dashboard slots

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import MagicButton from '@/app/components/magic-button';
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
