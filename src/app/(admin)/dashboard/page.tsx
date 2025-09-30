3fb60ef8e51ab70cf015e5c33eef7dd4966bedb2
Volodymyr <StuparenkoVladimir@gmail.com>
2025-09-30 12:58:42 +0300

    Add error handling components

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MagicButton from '@/app/components/magic-button';
 
 export interface PageProps {}
 
@@ -6,6 +7,7 @@ export default function Page({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
+      <MagicButton />
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AddCompanyButton from '@/app/components/add-company-button';
+import MagicButton from '@/app/components/magic-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
